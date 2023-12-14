9c89535ad2479b689eb7a5dda64b07934569ce42
Max <sultonbek.rakhimov@gmail.com>
2023-12-14 16:47:25 +0500

    Add initial API configurations

---
+++ src/MiniProjects.AirBnb.Backend/AirBnb.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirBnb.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static readonly ICollection<Assembly> Assemblies;
+
+    static HostConfiguration()
+    {
+        Assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+        Assemblies.Add(Assembly.GetExecutingAssembly());
+    }
+
+    private static WebApplicationBuilder AddMappers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(Assemblies);
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
---
+++ src/MiniProjects.AirBnb.Backend/AirBnb.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace AirBnb.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder.AddMappers().AddExposers();
+
+        return new(builder);
+    }
+
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app.UseExposers();
+
+        return new(app);
+    }
+}
