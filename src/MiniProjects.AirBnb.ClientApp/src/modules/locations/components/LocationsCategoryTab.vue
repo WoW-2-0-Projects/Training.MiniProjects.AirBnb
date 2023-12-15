270f86b9a0f34b0884e46ca2048851ab83b44f9e
Max <sultonbek.rakhimov@gmail.com>
2023-12-15 17:54:11 +0500

    Add locations category tab

---
+++ src/MiniProjects.AirBnb.ClientApp/src/modules/locations/components/LocationsCategoryTab.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <div class="fixed w-full top-0 pt-2 mt-20 z-10 bg-white flex items-center justify-center gap-4 content-padding">
+    
+    </div>
+
+</template>
--- src/MiniProjects.AirBnb.ClientApp/src/modules/locations/components/LocationsContainer.vue
+++ src/MiniProjects.AirBnb.ClientApp/src/modules/locations/components/LocationsContainer.vue
@@ -2,6 +2,12 @@
 
     <div class="flex flex-col items-center justify-center">
 
+        <!-- Locations tab -->
+        <locations-category-tab/>
+
     </div>
 
 </template>
+<script setup lang="ts">
+import LocationsCategoryTab from "@/modules/locations/components/LocationsCategoryTab.vue";
+</script>
